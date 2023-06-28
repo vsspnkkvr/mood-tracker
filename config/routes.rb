bfaf4b7766e605b44db6ff033a7bf99a56319266
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-06-28 14:49:04 -0400

    v1.1 base application fuction and structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :days
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :days do
+    resources :notes
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_232957) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_022727) do
   create_table "days", force: :cascade do |t|
     t.date "mooddate"
     t.integer "moodrating"
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_232957) do
     t.string "moodword"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "day_id"
+    t.index ["day_id"], name: "index_notes_on_day_id"
   end
 
 end
