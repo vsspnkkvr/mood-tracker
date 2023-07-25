a311c7c2b490439918e0fee76d163f95588b07b4
gacurl <greg.a.curl@gmail.com>
2023-07-25 18:42:03 -0400

    add authenticate_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    private
     before_action :set_query
+
+    def authenticate_user!
+        if user_signed_in?
+            root_path
+        else
+            render 'days/index', notice: "Please Login to view that page!"
+        end
+    end
 
     def set_query
         @q = Day.ransack(params[:q])
